9601648ed479a0356ffc99ecd4ec22e1b018c189
david nakayama <david.m.nakayama@vanderbilt.edu>
2019-07-23 21:46:47 -0700

    Maybe this will fix it.

--- config/environment.js
+++ config/environment.js
@@ -25,7 +25,7 @@ module.exports = function(environment) {
 
     blog: {
       title: "Michael Nakayama",
-      description: "Developer / Designer / Entreprenuer",
+      description: "Developer / Manager / Designer / Entreprenuer",
       coverImage: "/images/blog-cover.jpg",
 
       navigation: [
